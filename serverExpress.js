cf9fd295fed6a141f44a4ce8da652177b6db0cfb
jesbell <joselyngonzalezma@gmail.com>
2024-03-29 01:22:02 -0300

    Se realiza la devolución de Json con un arreglo de nombres

---
+++ serverExpress.js
@@ -0,0 +1,34 @@
+// Creando instancia express
+const express = require('express');
+// Guardamos en una constante app 
+const app = express();
+
+// Se crea un arreglo de nombres
+const usuarios = ['Juan', 'Jocelyn', 'Astrid', 'MAria', 'Ignacia', 'Javier', 'brian'];
+
+// Creando servidor con express en puerto 3000
+app.listen(3000, () => {
+    console.log('El servidor está inicializado en el puerto 3000');
+});
+
+const path = require("path");
+//Definiendo carpeta "assets" como pública
+app.use(express.static(path.join(__dirname, "assets")));
+
+// Devolviendo arreglo usuarios en formato Json
+app.get('/usuarios', (req, res) => {
+    res.json({usuarios: usuarios});
+});
+
+// Agregando middleware
+app.use("/abracadabra/juego/:usuario", (req, res, next) => {
+    const usuario = req.params.usuario;
+
+});
+
+
+
+
+app.get("*", (req, res) => {
+    res.send("<center><h1>Sorry, aquí no ha nada :/ </h1></center>");
+});
